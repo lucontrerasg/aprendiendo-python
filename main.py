ff839fee9be567be316eae382138e68caad32dae
Luis Barrera Abarca <lu.barreraabarca@gmail.com>
2023-07-27 21:27:25 -0400

    primer commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+print('Hola Luisa y Luis soy su primer codigo')
+
+# suma
+x = 1
+y = 2
+z = x + y  # 1 + 2
+print(f'z = {z}')
+# multiplicacion
+a = x * y # 1 * 2
+print(f'a = {a}')
+# resta
+b = x - y # 1 - 2
+print(f'b = {b}')
+
+# Crear una variable c que sea la division entre a y b
+#division
+
+a=5
+b=4
+c= a/b 
+print (f'c = {c}')
+
+# Tipo de datos
+print(type(1))
+print(type(1.3))
+print(type('texto'))
+print(type('&'))
+print(type('/'))
